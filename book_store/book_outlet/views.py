d6bc54687fa38e05753c96b4b8dbadf476acd637
aguttero <aguttero@gmail.com>
2025-12-04 20:34:10 -0300

    sql lite setup v01

---
+++ book_store/book_outlet/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "book_outlet/index.html")
+
+def detail(request):
+    return render (request, "book_outlet/book_detail.html")
+    
+

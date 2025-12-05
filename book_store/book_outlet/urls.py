6319376782c9400ee66397bb982e57c4619b95af
aguttero <aguttero@gmail.com>
2025-12-05 16:55:58 -0300

    setup db model and index page

--- book_store/book_outlet/urls.py
+++ book_store/book_outlet/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
+    path("<int:book_db_id>", views.book_detail, name="book_detail_url"),
     path("detail", views.detail)
 ]
--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+
+from .models import Book
 
 # Create your views here.
 def index(request):
-    return render(request, "book_outlet/index.html")
+    books = Book.objects.all()
+    return render(request, "book_outlet/index.html", {
+        "books": books
+    })
 
+def book_detail(request, book_db_id):
+    # try:
+    #     book = Book.objects.get(id=book_db_id)
+    #     # book = Book.objects.get(pk=book_db_id) pk: primary key
+    # except:
+    #     raise Http404()
+    book = get_object_or_404(Book, pk=book_db_id)
+    return render (request, "book_outlet/book_detail.html", {
+            "book": book
+        })
+
+## dummy data 
 def detail(request):
     return render (request, "book_outlet/book_detail.html")
-    
 
